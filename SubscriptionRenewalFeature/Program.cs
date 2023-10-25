0273f59fd766a40368b99c5506dc1d94e30b1946
Kiongo-Bob <njoguub@gmail.com>
2023-10-25 19:34:43 +0300

    Subscription renewal feature

---
+++ SubscriptionRenewalFeature/Program.cs
@@ -0,0 +1,24 @@
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+Console.WriteLine(daysUntilExpiration);
+
+if (daysUntilExpiration == 1)
+{
+    discountPercentage = 20;
+    Console.WriteLine($"Your subscription will expire within a day.\nRenew now and save {discountPercentage}%!");    
+}
+else if (daysUntilExpiration <= 5)
+{
+    discountPercentage = 10;
+    Console.WriteLine($"Your subscription will in {daysUntilExpiration} days.\nRenew now and save {discountPercentage}%!");
+}
+else if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+else
+{
+    Console.WriteLine("Your subscription has expired.");
+}
