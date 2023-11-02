e0eb3e6cc78fd9b7119b339824bce3b444274c09
Kiongo-Bob <njoguub@gmail.com>
2023-11-02 12:00:20 +0300

    Rolling dice & OrderIds: Arrays and random

---
+++ PalindromeChecker/Program.cs
@@ -0,0 +1,15 @@
+﻿string word = "RACECA";
+
+float length = word.Length;
+float midpointFloat = (length)/2;
+int midpointInt = word.Length/2;
+
+int startCurrent = 0;
+int lastCurrent = lengthInt;
+
+if (midpointFloat > midpointInt)
+{
+    midpointInt += 1;
+}
+
+Console.WriteLine(midpointFloat+" "+ midpointInt);
